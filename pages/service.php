3f0efc96f86ccd65a8641c44763b5702e641b69c
Qendresa Osmani <qendresa.osmani17@outlook.com>
2024-02-22 20:46:51 +0100

    editing service.php

--- pages/service.php
+++ pages/service.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Service Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-    <link
-        href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&family=Open+Sans:wght@300;400;500;600;700;800&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@400;700;800&family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/0fcaa0e277.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/footer.css">
+    <style>
+        body {
+            font-family: 'Prata', serif;
+            background-color: #F9F5F3;
+        }
+    </style>
 
 </head>
+
 <body>
-<div class="container container-first">
-        <header
-            class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom border-2">
-            <a href="../index.html"
-                class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <div class="container container-first">
+        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom border-2">
+            <a href="../index.html" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 <img src="../images/logo.png" class="ms-3 img-fluid " alt="" style="height: 50px; width: 50px;">
             </a>
             <div class="navbar navbar-expand-lg">
                 <div class="container-fluid ">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse " id="navbarNav">
@@ -44,8 +44,7 @@
                                     <a class="nav-link px-4" href="./products.html">Products</a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
-                                        role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         About
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -61,8 +60,7 @@
                             </ul>
                         </div>
                         <div>
-                            <a class="wishlist" href="./pages/wishlist.html"><i
-                                    class="fa-regular fa-heart me-4"></i></a>
+                            <a class="wishlist" href="./pages/wishlist.html"><i class="fa-regular fa-heart me-4"></i></a>
                             <a href="signUp.html"><button type="button" class="btn btn-primary">Sign-up</button></a>
                         </div>
                     </div>
@@ -72,10 +70,10 @@
     </div>
 
 
-<!--table with data-->
+    <!--table with data-->
     <div class="container">
         <h1>Service Page</h1>
-        <table class="table table-striped">
+        <table class="table table-striped table-bordered table-hover  ">
             <thead>
                 <tr>
                     <th>ID</th>
@@ -126,7 +124,7 @@
     </div>
 
     <!--footer-->
-    <footer class="footer flex-shrink-0 ">
+    <footer class="footer flex-shrink-0 " style="margin-top: 100px">
         <div class="container">
             <div class="row ">
                 <div class="footer-col col-xl-3 col-md-6 col-sm-12">
@@ -169,4 +167,5 @@
         </div>
     </footer>
 </body>
+
 </html>
